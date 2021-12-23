d421681b952c527e8eb804d3c6b6fa8e7f5419b1
Adam Martiska <adam.martiska@kistler.com>
2021-12-24 00:08:55 +0100

    Add card prototype for food menu how should it look like in v1

--- GUI/eMenu-customer/src/pages/food-menu-page.js
+++ GUI/eMenu-customer/src/pages/food-menu-page.js
@@ -1,8 +1,14 @@
 import React from "react";
-import MenuItem3 from "../components/MenuItem3";
 import { useDispatch, useSelector } from "react-redux";
+import Card from "@mui/material/Card";
+import Box from "@mui/material/Box";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
 import { addToCart } from "./drinksSlice";
 import { useMealsQuery } from "../generated/graphql";
+import "./food-menu-page.scss";
+import burgerUrl from "../static/burger.jpeg";
 
 const FoodMenuPage = () => {
   const { data, error, loading } = useMealsQuery();
@@ -38,8 +44,8 @@ const FoodMenuPage = () => {
     [mealOrder, dispatch]
   );
   return (
-    <div className="drinks-menu-page-wrapper">
-      {data?.meals.map(({ id, name, price }) => (
+    <div className="food-menu-page-wrapper">
+      {/* {data?.meals.map(({ id, name, price }) => (
         <MenuItem3
           key={id}
           id={id}
@@ -50,7 +56,32 @@ const FoodMenuPage = () => {
           count={mealOrder.cocaCola}
           price={price}
         />
-      ))}
+      ))} */}
+      <Card sx={{ display: "flex", height: 95, paddingVertical: 10 }}>
+        <Box>
+          <CardContent sx={{ display: "flex", flexDirection: "row" }}>
+            <div style={{ marginRight: 20 }}>
+              <Typography component="div" variant="h6">
+                Kralovsky burger
+              </Typography>
+              <Typography
+                variant="subtitle2"
+                color="text.secondary"
+                component="div"
+              >
+                100 % hovadzie maso, domaca slaninka, vajicko, majoneza, ponozky
+                a rozne ...
+              </Typography>
+            </div>
+            <CardMedia
+              component="img"
+              sx={{ width: 90, height: 70 }}
+              image={burgerUrl}
+              //alt="Live from space album cover"
+            />
+          </CardContent>
+        </Box>
+      </Card>
     </div>
   );
 };
