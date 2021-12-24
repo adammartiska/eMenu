62b73fcebc5acecb52daa51e66028f0160dbcd91
Adam Martiska <adam.martiska@kistler.com>
2021-12-24 11:43:25 +0100

    Add basic bottom drawer which opens when user clicks on some food

--- GUI/eMenu-customer/src/pages/food-menu-page.js
+++ GUI/eMenu-customer/src/pages/food-menu-page.js
@@ -4,11 +4,22 @@ import { addToCart } from "./drinksSlice";
 import { useMealsQuery } from "../generated/graphql";
 import "./food-menu-page.scss";
 import FoodCard from "../components/FoodCard";
+import Drawer from "@mui/material/Drawer";
 
 const FoodMenuPage = () => {
   const { data, error, loading } = useMealsQuery();
   const [mealOrder, setMealOrder] = React.useState({});
+  const [showDrawer, setShowDrawer] = React.useState(false);
+  const [currentlyOpenedMealId, setCurrentlyOpenedMealId] =
+    React.useState(null);
   const dispatch = useDispatch();
+
+  const toggleDrawer =
+    (open, id = null) =>
+    (event) => {
+      setShowDrawer(open);
+      setCurrentlyOpenedMealId(id);
+    };
 
   const handleAddButtonClick = React.useCallback(
     (id) =>
@@ -50,9 +61,17 @@ const FoodMenuPage = () => {
           title={name}
           count={mealOrder.cocaCola}
           price={price}
-          onClick={(id) => console.log(id)}
+          onClick={toggleDrawer(true, id)}
+          //onClick={(id) => console.log(id)}
         />
       ))}
+      <React.Fragment>
+        <Drawer anchor="bottom" open={showDrawer} onClose={toggleDrawer(false)}>
+          <div style={{ height: 80 }}>
+            ahojky otvoril si {currentlyOpenedMealId}
+          </div>
+        </Drawer>
+      </React.Fragment>
     </div>
   );
 };
